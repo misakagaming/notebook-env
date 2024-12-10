17b5e19c281bcf308ededafb6bb27f911c70b463
Eray Erer <eray.erer@hotmail.com>
2024-12-10 20:16:16 +0300

    fixed y_train

--- containers/notebook-env/notebooks/gan_fraud_detection_esmote.py
+++ containers/notebook-env/notebooks/gan_fraud_detection_esmote.py
@@ -208,7 +208,7 @@ scaler = StandardScaler()
 X = scaler.fit_transform(df.drop(columns='Class'))
 y = df['Class'].values
 
-X_train, X_test, X_train, y_test = train_test_split(X, y, test_size=0.05, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, stratify=y)
 
 
 train_combined = pd.DataFrame(X_train)
