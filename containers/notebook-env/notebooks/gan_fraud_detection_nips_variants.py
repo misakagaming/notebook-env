ea424c2deb317359165d0f4f3387d48afb66b919
Eray Erer <eray.erer@hotmail.com>
2025-02-06 13:36:29 +0300

    concept drift

--- containers/notebook-env/notebooks/gan_fraud_detection_nips_variants.py
+++ containers/notebook-env/notebooks/gan_fraud_detection_nips_variants.py
@@ -441,7 +441,7 @@ for data_name in ["cct"]:
       X = scaler.fit_transform(df.drop(columns='isFraud'))
       y = df['isFraud'].values
     if data_name == "cct" and conceptDrift:
-      test_cd = df.loc[df["Month"].isin([months[start+8]])]
+      test_cd = df[df["Month"]==months[start+8]]
       train_cd = df.loc[df["Month"].isin(months[start:start+8])]
       X_train = scaler.fit_transform(train_cd.drop(columns='Is Fraud?'))
       y_train = train_cd['Is Fraud?'].values
